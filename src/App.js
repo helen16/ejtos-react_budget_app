b1a4064d47a22f74d0c8440d4cd42af2371fdf20
helen16 <keep.in.mind32@gmail.com>
2024-02-15 12:00:44 -0500

    dropdown ist ready

--- src/App.js
+++ src/App.js
@@ -7,42 +7,44 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
-
+import Currency from './components/Currency';
 
 import { AppProvider } from './context/AppContext';
-
-
 
 const App = () => {
     return (
         <AppProvider>
-        <div className='container'>
-            <h1 className='mt-3'>Company's Budget Allocation</h1>
-            <div className='row mt-3'>
-                <div className='col-sm'>
-                    <Budget />
+            <div className='container'>
+                <h1 className='mt-3'>Company's Budget Allocation</h1>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                    <div className='col-sm'>
+                        <RemainingBudget />
+                    </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>
                 </div>
-                <div className='col-sm'>
-                    <RemainingBudget />
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
                 </div>
-                <div className='col-sm'>
-                    <ExpenseTotal />
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
-            <h3 className='mt-3'>Allocation</h3>
-            <div className='row '>
-                <div className='col-sm'>
-                    <ExpenseList />
-                </div>
-            </div>
-            <h3 className='mt-3'>Change allocation</h3>
-            <div className='row mt-3'>
-                <div className='col-sm'>
-                    <AllocationForm/>
-                </div>
-            </div>
-        </div>
-    </AppProvider>
+        </AppProvider>
     );
 };
+
 export default App;
---
+++ src/components/Currency.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Currency = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const handleChange = (event) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value,
+        });
+    };
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Currency: </span>
+            <select onChange={handleChange}>
+                <option value="$">Dollar ($)</option>
+                <option value="£">Pound (£)</option>
+                <option value="€">Euro (€)</option>
+                <option value="₹">Rupee (₹)</option>
+            </select>
+        </div>
+    );
+};
+
+export default Currency;
